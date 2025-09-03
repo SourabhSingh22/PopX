f0aaf2d54af1b1d3d08a1c80cbf9d86c1bb98aa3
Saurabh <sourabhsingh22112003@gmail.com>
2025-09-03 18:05:40 +0530

    make responsive

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const HomePage = () => {
     return (
-        <div className='w-[385px] sm:w-[375px]  h-[100vh] sm:h-[100vh] shadow-md relative flex'>
-            <div className='align-left w-[242px] sm:h-[48px] absolute top-[330px] left-[20px]'>
-                <h1 className='text-[28px] font-bold mb-2'>Welcome to PopX</h1>
+        <div className='w-[385px] sm:w-[375px]  h-[100vh] sm:h-[100vh] shadow-md relative'>
+            <div className='align-left w-[242px] sm:h-[48px] h-[48px] absolute top-[330px] left-[20px]'>
+                <h1 className='sm:text-[28px] text-4xl font-bold sm:mb-2'>Welcome to PopX</h1>
                 <p className='text-[18px] text-gray-500'>Loream ipsum dolor sit amet, consectetur adipiscing elit,</p>
             </div>
 
