153ed7896efae7fb590028c51426fb7a5249d570
Saurabh <sourabhsingh22112003@gmail.com>
2025-09-03 16:35:52 +0530

    commit

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const HomePage = () => {
     return (
-        <div className='w-[375px] h-[100vh] shadow-md relative'>
-            <div className='align-left w-[242px] h-[48px] absolute top-[330px] left-[20px]'>
+        <div className='w-[375px] h-[100vh] sm:h-[100vh] shadow-md relative'>
+            <div className='align-left w-[242px] sm:h-[48px] absolute top-[330px] left-[20px]'>
                 <h1 className='text-[28px] font-bold mb-2'>Welcome to PopX</h1>
                 <p className='text-[18px] text-gray-500'>Loream ipsum dolor sit amet, consectetur adipiscing elit,</p>
             </div>
